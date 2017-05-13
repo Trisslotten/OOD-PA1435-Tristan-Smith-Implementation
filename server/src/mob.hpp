02fadf21d9d87980ada8d4da510d5b15de1311ba
Trisslotten <tristan23@live.se>
2017-05-13 16:46:21 +0200

    fix 2

--- server/src/mob.hpp
+++ server/src/mob.hpp
@@ -7,8 +7,7 @@
 
 class Mob
 {
-// TODO: change to protected or private
-public:
+private:
 	ID id;
 	std::string name;
 	std::string description;
@@ -17,7 +16,69 @@ public:
 	int health, max_health;
 
 	sf::Vector2i pos;
-
-	Mob();
-	Mob(ID id);
+public:
+	inline ID getID() const
+	{
+		return id;
+	}
+	inline std::string getName() const
+	{
+		return name;
+	}
+	inline std::string getDescription() const
+	{
+		return description;
+	}
+	inline char getSymbol() const
+	{
+		return symbol;
+	}
+	inline sf::Color getColor() const
+	{
+		return color;
+	}
+	inline int getHealth() const
+	{
+		return health;
+	}
+	inline int getMaxHealth()const
+	{
+		return max_health;
+	}
+	inline sf::Vector2i getPos()const
+	{
+		return pos;
+	}
+	inline void setID(ID _id)
+	{
+		id = _id;
+	}
+	inline void setName(std::string n)
+	{
+		name = n;
+	}
+	inline void setDescription(std::string desc)
+	{
+		description = desc;
+	}
+	inline void setSymbol(char sym)
+	{
+		symbol = sym;
+	}
+	inline void setColor(sf::Color col)
+	{
+		color = col;
+	}
+	inline void setHealth(int i)
+	{
+		health = i;
+	}
+	inline void setMaxHealth(int max)
+	{
+		max_health = max;
+	}
+	inline void setPos(sf::Vector2i p)
+	{
+		pos = p;
+	}
 };
